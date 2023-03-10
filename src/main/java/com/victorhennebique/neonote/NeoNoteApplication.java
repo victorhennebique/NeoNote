6837adae6604758d9e2e7152034cf199db9beb26
victorhennebique <victor.hennebique@orange.fr>
2023-03-10 21:15:48 +0100

    Add files of project

---
+++ src/main/java/com/victorhennebique/neonote/NeoNoteApplication.java
@@ -0,0 +1,13 @@
+package com.victorhennebique.neonote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoNoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victorhennebique/neonote/NeoNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorhennebique.neonote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
